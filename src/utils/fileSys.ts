ccf989364911a796178d19a64cc9387bfa964fdb
coooo77 <coooo77@hotmail.com>
2024-12-22 23:03:00 +0800

    refactor: remove invalid code

--- src/utils/fileSys.ts
+++ src/utils/fileSys.ts
@@ -2,7 +2,6 @@
 
 import fs from 'fs'
 import path from 'path'
-import { fileURLToPath } from 'url'
 import { AppSettings, RecordingList, UsersList } from '../interfaces/index.js'
 
 export default {
