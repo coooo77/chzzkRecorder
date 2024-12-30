7fa16f501f2269c1321765cee90d9b0ffc145e1f
coooo77 <coooo77@hotmail.com>
2024-12-31 00:56:36 +0800

    feat: data watcher

--- src/utils/model.ts
+++ src/utils/model.ts
@@ -1,8 +1,11 @@
 'use strict'
+import path from 'path'
+import chokidar from 'chokidar'
 import EventEmitter from 'events'
 
 import { RecordEvent } from './recorder.js'
 
+import helper from './common.js'
 import fileSys from './fileSys.js'
 
 import type { UsersList, AuthCookie, AppSettings, UserSetting, RecordingList } from '../interfaces/index.js'
@@ -27,6 +30,8 @@ export interface ModelEventMap {
 }
 
 export default class Model extends EventEmitter<ModelEventMap> {
+  isWatchOn = false
+
   mutex = Promise.resolve()
 
   appSetting: AppSettings
@@ -88,8 +93,57 @@ export default class Model extends EventEmitter<ModelEventMap> {
   }
   //#endregion
 
+  // #region 資料同步
+  async init() {
+    await Promise.all([this.updateCookie(), this.updateUserList(), this.initRecordList(), fileSys.makeDirIfNotExist(this.appSetting.saveDirectory)])
+  }
+
+  async syncModel() {
+    const [userList, appSetting, authCookie] = await Promise.all([fileSys.getUsersList(), fileSys.getAppSetting(), fileSys.getCookie()])
+
+    this.userList = userList
+    this.authCookie = authCookie
+    this.appSetting = appSetting
+  }
+
+  /** 精读《如何利用 Nodejs 监听文件夹》 @see https://tinyurl.com/n9r7p3mk */
+  watchModel() {
+    const { cookiePath, appConfigPath, usersListPath } = fileSys
+    const watchList = [cookiePath, appConfigPath, usersListPath]
+    const [cookie, setting, userList] = watchList.map((p) => path.basename(p))
+
+    const method = {
+      [cookie]: () => {
+        helper.msg('Cookie updated')
+        this.updateCookie()
+      },
+      [userList]: () => {
+        helper.msg('User List updated')
+        this.updateUserList()
+      },
+      [setting]: () => {
+        helper.msg('App Setting updated')
+        this.updateAppSetting()
+      },
+    }
+
+    chokidar.watch(watchList).on('change', (p) => {
+      const name = path.basename(p)
+      if (name in method) method[name as keyof typeof method]()
+    })
+  }
+
+  async updateAppSetting() {
+    this.appSetting = await fileSys.getAppSetting()
+  }
+
+  async updateUserList() {
+    this.userList = await fileSys.getUsersList()
+  }
+  // #endregion
+
   //#region Cookie
-  async initCookie() {
+  async updateCookie() {
     const cookie = await fileSys.getCookie({ init: true })
     if (cookie) this.authCookie = cookie
   }
@@ -112,10 +166,6 @@ export default class Model extends EventEmitter<ModelEventMap> {
     return this.mutex as Promise<ReturnType<F>>
   }
 
-  async init() {
-    await Promise.all([this.initCookie(), this.initUserList(), this.initRecordList(), fileSys.makeDirIfNotExist(this.appSetting.saveDirectory)])
-  }
-
   async getAppSetting() {
     if (this.appSetting) return this.appSetting
 
@@ -124,16 +174,5 @@ export default class Model extends EventEmitter<ModelEventMap> {
     return this.appSetting
   }
 
-  async syncModel() {
-    const [userList, appSetting, authCookie] = await Promise.all([fileSys.getUsersList(), fileSys.getAppSetting(), fileSys.getCookie()])
-
-    this.userList = userList
-    this.authCookie = authCookie
-    this.appSetting = appSetting
-  }
-
-  async initUserList() {
-    this.userList = await fileSys.getUsersList()
-  }
   //#endregion
 }
